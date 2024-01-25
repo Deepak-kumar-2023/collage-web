f654051137f3f5666a4fc51e0ab3a2dc5beb0823
Deepak <kumardeeepak392@gmail.com>
2024-01-25 23:47:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// console.log("i am a good boy");
+// function sayHello(nums1,nums2) {
+//     const sum=nums1+nums2;
+//     return sum
+// }
+// function sayMyName() {
+//     console.log("de")
+//     console.log("ep")
+//     console.log("ak")
+// }
+// console.log(sayHello("result: "+2,5))
+// sayMyName()
+// function loginUserMessage(username="sam") {
+//     if(!username){
+//         console.log("please enter a username")
+//         return;
+//     }
+//     return `${username} just logged in`
+// }
+
+// function claculateCardPrice(...num) {
+//     return num;
+// }
+// console.log(claculateCardPrice(100,200,300,400));
+// console.log(loginUserMessage(""));
+// console.log(loginUserMessage("deepak"));
+
+// const user1={
+//     username:"deepak",
+//     roll_no:2306155,
+//     branch:"cse"
+// }
+// const user2={
+//     username:"shubham",
+//     roll_no:2306154,
+//     branch:"cse"
+// }
+// const user3={
+//     username:"anshu",
+//     roll_no:2306105,
+//     branch:"cse"
+// }
+// function handleOnject(anyObject) {
+//     console.log(`username is ${anyObject.username} and roll no is${anyObject.roll_no} and branch is ${anyObject.branch}`);
+
+// }
+// // console.log(handleOnject(user1))
+// handleOnject({username:"vivek",
+// roll_no:"230101",
+// branch:"cse"})
