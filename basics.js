87272ab3e9d504e6c52b122cac7c0ae3a61ae3ff
Deepak <kumardeeepak392@gmail.com>
2024-01-26 17:52:23 +0530

    basics02

---
+++ basics.js
@@ -0,0 +1,25 @@
+// let one=1;
+// var two=2;
+// const three=3;
+// console.log(one);
+// console.log(two);
+// console.log(three);
+
+// let a=300
+// if(true){
+//     let a=10;
+//     let b=20;
+//     console.log("inner: "+a);
+// }
+// console.log(a)
+
+function one() {
+    const username="deepak_kumar"
+function two() {
+    const website="youtube"
+}
+//console.log(website);  //con not excess website --> show error
+// two()
+}
+// one()
+
